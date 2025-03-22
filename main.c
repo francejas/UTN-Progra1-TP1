79f9b179f43da2af1b8e181d54ff5a5b279cf69b
Francisco Cejas <138420593+francejas@users.noreply.github.com>
2025-03-22 02:33:42 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,132 +1,120 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>  // Necesario para la función time()
+#include "pila.h"
 
-// PROTOTIPADO
-int numRand();
-void mostrarMayorYMenor(int a, int b, int c);
-int sumaEnterosMenores(int N);
-void tablaMultiplicar(int a);
-float suma (float a ,float b);
-float resta (float a ,float b);
-float multiplicacion (float a ,float b);
-float division (float a ,float b);
-float convertirNegativo (float *a);
+// Prototipos de funciones
 int menu();
+void cargarPILA(Pila *pila);
+void pasarPILA(Pila *origen);
+void pasarPilaOrdenada(Pila *origen);
+int encontrarMenor (Pila *pila);
+void ordenamientoSeleccion (Pila *origen);
+int encontrarMenorElemento (Pila *pila);
+
 
 int main(int argc, char *argv[]) {
     int selected;
+    Pila pila0,pilaOrdenadaMayorAMenor,pilaOrdenadaMenorAMayor;
+
+    inicpila(&pila0);
+    inicpila(&pilaOrdenadaMayorAMenor);
+    inicpila(&pilaOrdenadaMenorAMayor);
+
+    apilar(&pilaOrdenadaMayorAMenor,6);
+    apilar(&pilaOrdenadaMayorAMenor,4);
+    apilar(&pilaOrdenadaMayorAMenor,3);
+    apilar(&pilaOrdenadaMayorAMenor,1);
+
+    apilar(&pilaOrdenadaMenorAMayor,1);
+    apilar(&pilaOrdenadaMenorAMayor,3);
+    apilar(&pilaOrdenadaMenorAMayor,4);
+    apilar(&pilaOrdenadaMenorAMayor,6);
+
 
     do {
         selected = menu();
-        switch(selected) {
+        switch (selected) {
             case 1:
-                {
-                    int randomNumber = numRand();
-                    printf("El numero random es: %d\n", randomNumber);
-                    system("PAUSE");
-                }
+                inicpila(&pila0);
+                cargarPILA(&pila0);
+                system("PAUSE");
                 break;
             case 2:
-                {
-                    int num1, num2, num3;
-                    printf("Ingrese 3 numeros enteros: ");
-                    scanf("%d %d %d", &num1, &num2, &num3);
-                    mostrarMayorYMenor(num1, num2, num3);
-                    system("PAUSE");
-                }
+                inicpila(&pila0);
+                cargarPILA(&pila0);
+                pasarPILA(&pila0);
+                system("PAUSE");
                 break;
             case 3:
-                {
-                    int N;
-                    printf("Ingrese un numero entero: ");
-                    scanf("%d", &N);
-                    int suma = sumaEnterosMenores(N);
-                    printf("La suma de los enteros menores que %d es: %d\n", N, suma);
-                    system("PAUSE");
-                }
-                break;  // Añadido el break para evitar caer en el caso 4
+                inicpila(&pila0);
+                cargarPILA(&pila0);
+                pasarPilaOrdenada(&pila0);
+                system("PAUSE");
+                break;
             case 4:
-                {
-                    int num1;
-                    printf("Ingrese un numero entero: ");
-                    scanf("%d", &num1);
-                    tablaMultiplicar(num1);
-                    system("PAUSE");
-                }
+                inicpila(&pila0);
+                cargarPILA(&pila0);
+                int menorElemento = encontrarMenor(&pila0);
+                printf("El menor elemento es: %d \n", menorElemento);
+                printf("La pila sin el menor elemento es: \n");
+                mostrar(&pila0);
+                system("PAUSE");
                 break;
             case 5:
-                {
-                    float num1, num2;
-                    char operador;
-
-                  printf("Ingrese el operador (+, -, *, / ): \n");
-                  scanf(" %c", &operador);
-
-                  printf("Ingrese primer numero: \n");
-                  scanf("%f",&num1);
-
-                  printf("Ingrese segundo numero: \n");
-                  scanf("%f",&num2);
-
-                  float resultado;
-
-                  switch (operador) {
-        case '+':
-            resultado = suma(num1, num2);
-            break;
-        case '-':
-    resultado = resta(num1, num2);
-            break;
-        case '*':
-    resultado = multiplicacion(num1, num2);
-            break;
-        case '/':
-    resultado = division(num1, num2);
-            break;
-        default:
-            printf("Operador invalido.\n");
-            break;
-    }
-
-    printf("Resultado: %.2f\n", resultado);
-    system("PAUSE");
-
-
-
-                }
-            break;
-        case 6:
-            {
-             float num;
-             printf("Ingrese un numero para convertirlo a negativo. \n");
-             scanf("%f",&num);
-             convertirNegativo(&num);
-             printf("Numero: %.2f \n", num);
-             system("PAUSE");
-            }
-            break;
-        case 7:
-            {
-                int num1, num2;
-                cargaValoresReferencia(&num1, &num2);
-                printf("Sus numeros: %d y %d.", num1,num2);
-
-             system("PAUSE");
-            }
-            break;
+                inicpila(&pila0);
+                cargarPILA(&pila0);
+                ordenamientoSeleccion(&pila0);
+                /*
+                printf("Pila origen:");
+                mostrar(&pila0);
+                */
+                system("PAUSE");
+                break;
+            case 6:
+                printf("Pila ordenada de Menor a Mayor");
+                mostrar(&pilaOrdenadaMenorAMayor);
+                int dato;
+                printf("Ingrese un dato a insertar en la pila: ");
+                scanf("%d", &dato);
+                insertarElemento(&pilaOrdenadaMenorAMayor, dato);
+                printf("Pila con el nuevo elemento insertado:\n");
+                mostrar(&pilaOrdenadaMenorAMayor);
+                system("PAUSE");
+                break;
+            case 7:
+                inicpila(&pila0);
+                cargarPILA(&pila0);
+                ordenamientoInsercion(&pila0);
+                printf("Pila ordenada por insercion:");
+                mostrar(&pila0);
+                system("PAUSE");
+                break;
+            case 8:
+                inicpila(&pila0);
+                cargarPILA(&pila0);
+                int suma=sumaTopeAnterior(pila0);
+                printf("La suma del tope y la base de la pila es: %d \n",suma);
+                system("PAUSE");
+                break;
+            case 9:
+                inicpila(&pila0);
+                cargarPILA(&pila0);
+                promedioPila(pila0);
+                system("PAUSE");
+                break;
             case 0:
                 printf("\n\nTERMINATE THE PROGRAM\n");
                 break;
             default:
-                printf("Opción no válida. Por favor, elige una opción del menú.\n");
+                printf("\nOpción no válida. Intente nuevamente.\n");
+                system("PAUSE");
                 break;
         }
-    } while(selected != 0);
+    } while (selected != 0);
 
-    system("PAUSE");
     return 0;
 }
+
 
 int menu() {
     system("color 1F");
@@ -136,89 +124,195 @@ int menu() {
     printf("\n----------");
     printf("\nLIST MENU");
     printf("\n-----------");
-    printf("\n1-Desarrollar una función que devuelva un valor RANDOM en el rango de 0 a 100");
-    printf("\n2-Diseñe una función que reciba 3 números enteros y muestre el mayor y el menor");
-    printf("\n3-Diseñe una función que reciba un número entero N y realice la suma de los números enteros positivos menores que N y lo retorne. N es un  dato ingresado por el usuario en el main");
-    printf("\n4-Desarrollar una función que muestre la tabla de multiplicar de un número entero recibido por parámetro.");
-    printf("\n5 Realice una pequeña calculadora, utilizando funciones una función de suma, una de multiplicación, una de resta, una de división…");
-    printf("\n6 Realizar una función que reciba un número positivo entero por parámetro por referencia, y cambie su signo a negativo.");
-    printf("\n7 Realizar una función que reciba dos números enteros por parámetro por referencia y cargue sus valores el usuario dentro de la función.");
-    printf("\n0-QUIT");
-    printf("\n\nENTER YOUR CHOICE: ");
+    printf("\n1- Ingresar varios elementos en una pila (cantidad indefinida por usuario).");
+    printf("\n2- Hacer una función que pase todos los elementos de una pila a otra.");
+    printf("\n3- Hacer una función que pase todos los elementos de una pila a otra, pero conservando el orden.");
+    printf("\n4- Hacer una función que encuentre el menor elemento de una pila y lo retorna. La misma debe eliminar ese dato de la pila.");
+    printf("\n5- Hacer una función que pase los elementos de una pila a otra, de manera que se genere una nueva pila ordenada. Usar la función del ejercicio 4. (Ordenamiento por selección).");
+    printf("\n6- Hacer una función que inserta en una pila ordenada un nuevo elemento, conservando el orden de ésta.");
+    printf("\n7- Hacer una función que pase los elementos de una pila a otra, de manera que se genere una nueva pila ordenada. Usar la función del ejercicio 6.  (Ordenamiento por inserción).");
+    printf("\n8- Hacer una función que sume los dos primeros elementos de una pila (tope y anterior), y retorne la suma,  sin alterar el contenido de la pila.");
+    printf("\n9- Hacer una función que calcule el promedio de los elementos de una pila, para ello hacer también una función que calcule la suma, otra para la cuenta y otra que divida. En total son cuatro funciones, y la función que calcula el promedio invoca a las otras 3.");
+    printf("\n0- SALIR");
+    printf("\n\nIngrese su elección: ");
     scanf("%d", &input);
     return input;
 }
 
-int numRand() {
-    system("cls");
-    system("color 1E");
-    srand(time(NULL));  // Inicializa la semilla para la generación de números aleatorios
-    int numeroRand = rand() % 101;  // Cambiado a 101 para incluir el 100 en el rango
-    return numeroRand;
-}
+void cargarPILA(Pila *pila){
+    char respuesta;
+    int dato;
+    do {
+        printf("\n Ingrese un numero  para la pila: \n");
+        fflush(stdin);
+        scanf("%d", &dato);
+        apilar(pila,dato);
+        printf("\n Desea ingresar otro numero? (s/n): ");
+        fflush(stdin);
+        scanf(" %c",&respuesta);
+    } while (respuesta == 's' || respuesta == 'S');
 
-void mostrarMayorYMenor(int a, int b, int c) {
-    int mayor, menor;
-
-    // Determinar el mayor
-    if (a >= b && a >= c) {
-        mayor = a;
-    } else if (b >= a && b >= c) {
-        mayor = b;
-    } else {
-        mayor = c;
-    }
-
-    // Determinar el menor
-    if (a <= b && a <= c) {
-        menor = a;
-    } else if (b <= a && b <= c) {
-        menor = b;
-    } else {
-        menor = c;
-    }
-
-    printf("El mayor es: %d\n", mayor);
-    printf("El menor es: %d\n", menor);
-}
-
-void tablaMultiplicar(int a) {
-    for (int i = 0; i <= 10; i++) {
-        int producto = a * i;
-        printf("%d x %d = %d\n", a, i, producto);
-    }
-}
-
-int sumaEnterosMenores(int N) {
-    int sumaMenores = 0;
-    for (int i = 1; i < N; i++) {  // Comienza desde 1 para sumar solo enteros positivos menores que N
-        sumaMenores += i;
-    }
-    return sumaMenores;
+    printf("\n Elementos en la pila después de la carga:\n");
+    mostrar(pila);  // Mostrar la pila después de la carga
 }
 
 
-float suma (float a ,float b) {
+void pasarPILA(Pila *origen){
+    Pila destino;
+    inicpila(&destino);
+
+    printf("Pila origen antes:\n");
+    mostrar(origen);
+
+    while (!pilavacia(origen)) {
+        apilar(&destino, desapilar(origen));
+    }
+    printf("Pila destino:\n");
+    mostrar(&destino);
+
+    printf("Pila origen despues:\n");
+    mostrar(origen);
+}
+
+void pasarPilaOrdenada(Pila *origen){
+    Pila destino, aux;
+
+    inicpila(&destino);
+    inicpila(&aux);
+
+    printf("Pila origen antes:\n");
+    mostrar(origen);
+
+    while (!pilavacia(origen)){
+        apilar(&aux, desapilar(origen));
+    }
+    while(!pilavacia(&aux)){
+        apilar(&destino, desapilar(&aux));
+    }
+    printf("Pila destino:\n");
+    mostrar(&destino);
+
+    printf("Pila origen despues:\n");
+    mostrar(origen);
+}
+
+int encontrarMenor (Pila *pila){
+    Pila aux;
+    inicpila(&aux);
+    int menor=tope(pila);
+
+    //encontrar el menor
+    while(!pilavacia(pila)) {
+        if (tope(pila)<menor){
+            menor=tope(pila);
+        }
+        apilar(&aux, desapilar(pila));
+    }
+
+
+   // Reconstruir la pila SIN el menor
+    while (!pilavacia(&aux)) {
+        if (tope(&aux) != menor) {  // Solo apilar si NO es el menor
+            apilar(pila, desapilar(&aux));
+        } else {
+            desapilar(&aux);  // Eliminar el menor
+        }
+    }
+
+
+    return menor; // Devuelve el menor eliminado
+}
+
+// ORDENA DE MAYOR A MENOR
+void ordenamientoSeleccion (Pila *origen){
+    Pila destino;
+    inicpila (&destino);
+
+    while(!pilavacia(origen)){
+        int elemento=encontrarMenor(origen);
+        apilar(&destino, elemento);
+    }
+    printf("Pila destino ordenada:");
+    mostrar(&destino);
+
+    //paso elementos a la pila original
+    printf("PASO DE DESTINO A ORIGEN: ");
+    while (!pilavacia(&destino)) {
+        apilar(origen, desapilar(&destino));
+    }
+    printf("Pila destino:\n");
+    mostrar(&destino);
+    printf("Pila origen:");
+    mostrar(origen);
+}
+
+void insertarElemento(Pila *pila, int dato) {
+    Pila aux;
+    inicpila(&aux);
+
+    //tope(pila) > dato FUNCIONA SOLO CON PILAS ORDENADAS DE MENOR A MAYOR.
+    //tope(pila) < dato FUNCIONA SOLO CON PILAS ORDENADAS DE MAYOR A MENOR.
+    // SI LE  PASO A ESTA FUNCION  UN A PILA DESORDENADA, APILA EN PILA EL MENOR ELEMTO.
+    while (!pilavacia(pila) && tope(pila) > dato) {
+        apilar(&aux, desapilar(pila));
+    }
+
+    apilar(pila, dato);
+
+    while (!pilavacia(&aux)) {
+        apilar(pila, desapilar(&aux));
+    }
+}
+
+//ordena de mayor a menor
+void ordenamientoInsercion(Pila *pila){
+    Pila aux;
+    inicpila(&aux);
+
+    while(!pilavacia(pila)){
+        int dato=desapilar(pila);
+        insertarElemento(&aux,dato);
+    }
+    while(!pilavacia(&aux)){
+        apilar(pila, desapilar(&aux));
+    }
+}
+
+int sumaTopeAnterior (Pila pila){
+    int a,b;
+    a = desapilar(&pila);
+
+    while(!pilavacia(&pila)){
+        b = desapilar(&pila);
+    }
+
     return a+b;
 }
-float resta (float a ,float b) {
-    return a-b;
-}
-float multiplicacion (float a ,float b) {
-    return a*b;
-}
-float division (float a ,float b) {
-    return a/b;
+
+int promedioPila(Pila pila) {
+    int suma=sumaElementosPila(pila);
+    int n = cantidadElementos(pila);
+
+    return promedio;
 }
 
-float convertirNegativo (float *a) {
-    *a=*a *-1;
+int sumaElementosPila(Pila pila){
+     int suma;
+     while(!pilavacia){
+        suma=+desapilar(pila);
+     }
+
+    return suma;
 }
 
-int cargaValoresReferencia(int *a, int *b){
-    printf("Ingrese el primer numero: ");
-    scanf("%d",a);
-    printf("Ingrese el segundo numero: ");
-    scanf("%d",b);
+int cantidadElementos(Pila pila){
+    int n;
+
+    while(!pilavacia){
+
+
+    }
+
+    return n;
 
 }
